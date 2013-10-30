98b65f9bba019bf04cdd94f9534c23bffd6dc105
J. Quiroga <jq@terra.fake>
2013-10-30 00:13:31 -0700

    Last part of assignment 7 exercise 2 done

--- Assignment07/pimpmytext.js
+++ Assignment07/pimpmytext.js
@@ -35,10 +35,62 @@ function blinging(ctrlId, bolder) {
 	}
 }
 
+function setBackground(setBackGrnd){
+	if (setBackGrnd){
+		$("body").css("background-image","url('./background.jpg')");
+	}else{
+		$("body").css("background-image","none");
+	}
+}
+
 function snoopify(ctrlId){
     strText = $("#"+ctrlId).val().toUpperCase();
 	if (strText.indexOf("-IZZLE.") < 0){
 	  strText = strText.split(".").join("-izzle.");
 	} 
 	$("#"+ctrlId).val(strText);
+}
+
+function pigLatVowel(word){
+	return word+"-ay";
+}
+
+function pigLatCons(word){
+	var isVowel = false;
+	var i=0;
+	var consCluster = "";
+	var tempStr = word.toLowerCase();
+	while (!isVowel){
+		switch (tempStr[i]){
+			case "a":
+			case "e":
+			case "i":
+			case "o":
+			case "u":
+			  isVowel = true;
+			break;
+			default:
+			  consCluster += tempStr[i];
+	    }
+	    i++;
+	}
+	return word.substr(i-1, word.length)+consCluster+"-ay";
+}
+
+function latinify(ctrlId){
+	var arrWords = $("#"+ctrlId).val().split(" ");
+	for (var i=0; i<arrWords.length; i++){
+		switch (arrWords[i].charAt(0).toLowerCase()){
+			case "a":
+			case "e":
+			case "i":
+			case "o":
+			case "u":
+			  arrWords[i] = pigLatVowel(arrWords[i]);
+			break;
+			default:
+			  arrWords[i] = pigLatCons(arrWords[i]);
+		}
+	}
+	$("#"+ctrlId).val(arrWords.join(" "));
 }
